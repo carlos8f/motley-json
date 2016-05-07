59d8f02f2af20e91caeb61bcba8c86e933e8c9b9
Carlos Rodriguez <carlos@s8f.org>
2016-05-06 19:55:10 -0700

    use new console

--- db/unlockDb.js
+++ db/unlockDb.js
@@ -2,14 +2,14 @@ var lockfile = require('lockfile')
 
 module.exports = function container (get, set) {
   return function unlockDb (cb) {
-    get('vendor.console').log('motley-json: unlocking db...')
+    get('console').log('motley-json: unlocking db...')
     try {
       lockfile.unlockSync(get('conf.db.json').path + '.lock')
     }
     catch (e) {
       return cb(e)
     }
-    get('vendor.console').log('motley-json: db unlocked.')
+    get('console').log('motley-json: db unlocked.')
     setImmediate(cb)
   }
 }
